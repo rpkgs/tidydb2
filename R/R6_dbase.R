e4640469223b3cbe4d885b8ff457fe5274cfccf0
Dongdong Kong <kongdd@users.noreply.github.com>
2023-12-15 14:02:23 +0800

    add write function but untested

--- R/R6_dbase.R
+++ R/R6_dbase.R
@@ -1,18 +1,31 @@
-#' dbase
+#' Database class for duckdb and sqlite
+#' 
+#' @field db a character string specifying a database name.
+#' @field table a character string specifying a table name.
+#' @field con a database connection object.
+#' @field tbl a `dplyr::tbl` object.
+#'
+#' @param db a character string specifying a database name.
+#' @param table a character string specifying a table name.
+#' @param tables a character vector specifying table names.
+#'
+#' @param overwrite a logical specifying whether to overwrite an existing table or not. Its default is FALSE.
+#' @param append a logical specifying whether to append to an existing table in the DBMS. Its default is FALSE.
+#' 
 #' @import R6
 #' @importFrom duckdb duckdb duckdb_shutdown
 #' @export
-dbase <- R6Class("duckdb_base", list(
+dbase <- R6Class("dbase", list(
   db = NULL,
   table = NULL,
-  # type = "duckdb",
   con = NULL,
   tbl = NULL,
+  
+  #' @return NULL
   initialize = function(db = NULL, table = NULL) {
     dbinfo <- get_dbInfo("duckdb")[[1]] # 默认是第一个
     if (is.null(db)) db <- dbinfo$db
     self$db <- db
-
     # type <- match.arg(type)
     # self$type <- type
     type = tools::file_ext(db)
@@ -23,11 +36,15 @@ dbase <- R6Class("duckdb_base", list(
     }
     self$open_table(table)
   },
+
+  #' @return NULL
   open_table = function(table) {
     if (is.null(table)) table <- DBI::dbListTables(self$con)[1]
     self$table <- table
     self$tbl <- tbl(self$con, self$table)
   },
+
+  #' @param ... ignored
   print = function(...) {
     fun = \(x) cat(green(x))
     fun(sprintf("[db   ]: %s\n", self$db))
@@ -38,11 +55,17 @@ dbase <- R6Class("duckdb_base", list(
     fun(sprintf("[ALL   Tables]: %s\n", paste(tables, collapse = ", ")))
     print(self$tbl)
   },
+
+  #' close db
+  #' @return NULL
   finalize = function() {
     message("close datadb ...")
-    self$close(force = TRUE)
+    self$close()
   },
-  close = function(force = FALSE) {
+
+  #' close db
+  #' @return NULL
+  close = function() {
     # if (!force) {
       DBI::dbDisconnect(self$con, shutdown = TRUE)
     # } else {
@@ -50,17 +73,23 @@ dbase <- R6Class("duckdb_base", list(
     # }
   },
 
+  #' Read Tables
+  #' @return list of `dataframe`
   read_tables = function(tables = NULL) {
     if (is.null(tables)) tables = DBI::dbListTables(db$con)
     tables %<>% set_names(., .)
     lapply(tables, \(table) collect(tbl(self$con, table)))
   },
 
+  #' Read Table
+  #' @return `dataframe`
   read_table = function(table = NULL) {
     if (!is.null(table)) self$open_table(table)
     self$read_data()
   },
 
+  #' @param ... `dplyr::filter` expressions
+  #' @param verbose a logical specifying whether to print the time or not.
   read_data = function(..., verbose = TRUE) {
     exprs <- rlang::enquos(...)
 
@@ -71,6 +100,30 @@ dbase <- R6Class("duckdb_base", list(
       if (verbose) print(t)
       d
     })
+  },
+
+  #' Write Table
+  #'
+  #' @param value `data.frame`
+  #' @param name  a character string specifying a table name. SQLite table names
+  #' are not case sensitive, e.g., table names ABC and abc are considered equal.
+  #' @param ... others to be passed to `dbWriteTable`
+  write_table = function(value, name = NULL, overwrite = TRUE, append = FALSE, ...) {
+    .name = deparse(substitute(value))
+    name %<>% `%||%`(.name)
+    if (append) overwrite = FALSE
+    dbWriteTable(self$con, name, value, overwrite = overwrite, append = append, ...)
+  },
+
+  #' Write Tables
+  #'
+  #' @param values a list of `data.frame`
+  #' @param names  a character vector specifying table names.
+  #' @param ... others to be passed to `dbWriteTable`
+  write_tables = function(values, names = NULL, overwrite = TRUE, append = FALSE, ...) {
+    names %<>% `%||%`(names(values))
+    for (i in seq_along(values)) {
+      self$write_table(names[i], values[[i]], overwrite, append, ...)
+    }
   }
-  ## add a write table options
 ))
--- scripts/test_dbase.R
+++ scripts/test_dbase.R
@@ -3,8 +3,8 @@ library(duckdb)
 # dbinfo$hourly_2020_2022
 
 db = dbase$new()
-d = db$read_data()       # first time about 20s, 16s可以读进来所有数据
-d <- db$read_data(50246) # 0.6s
+d <- db$read_data(site == 50246L) # first time about 20s, 16s可以读进来所有数据
+d <- db$read_data(site == 50246L) # 0.6s
 # db$close(force=TRUE)
 
 f = "z:/DATA/China/ChinaRunoff_latest/ChinaWater_latest/back_up/ChinaWater_river (20200603H22-20210116H14).db"
